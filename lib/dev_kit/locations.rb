82b05ebfd9c4b22da52a3fddd74287ea57b20580
Luis Lavena <luislavena@gmail.com>
2014-08-20 21:46:44 -0300

    Allow override of version_file in locations
    This will allow reuse of same VersionFile instance.
    
    Reverse arguments for version_file support

--- lib/dev_kit/locations.rb
+++ lib/dev_kit/locations.rb
@@ -4,9 +4,10 @@ module DevKit
   class Locations
     attr_reader :ruby_root
 
-    def initialize(ruby_exe = Gem.ruby)
+    def initialize(version_file = nil, ruby_exe = Gem.ruby)
       # path/bin/ruby.exe
-      @ruby_root = File.expand_path("../..", ruby_exe)
+      @ruby_root    = File.expand_path("../..", ruby_exe)
+      @version_file = version_file
     end
 
     def all
@@ -20,7 +21,7 @@ module DevKit
       return @default_dir if @default_dir
 
       base_dir = File.expand_path(ENV["ProgramData"] || ENV["ALLUSERSPROFILE"])
-      required_version = VersionFile.new(ruby_root).version
+      required_version = version_file.version
 
       path = [
         base_dir,
@@ -34,6 +35,12 @@ module DevKit
 
     def ruby_relative
       File.join(ruby_root, "DevKit")
+    end
+
+    private
+
+    def version_file
+      @version_file ||= VersionFile.new(ruby_root)
     end
   end
 end
